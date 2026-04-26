f59b0d2373cc70d4399ddf459aa4f5553acc8bed
Lean-Monster <LucaVozzolo16@gmail.com>
2026-04-26 16:59:10 +0100

    added input validation for grid specification and win condition

--- src/main/java/LucaVozzolo/Connect4/Main.java
+++ src/main/java/LucaVozzolo/Connect4/Main.java
@@ -1,5 +1,6 @@
 package LucaVozzolo.Connect4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,13 +8,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Connect4!");//simple start menu to set out grid size and win condition
         System.out.println("Enter the number of rows in the grid: ");
-        int rows = sc.nextInt();
+        int rows = inputValidator(sc);
         System.out.println("Enter the number of columns in the grid: ");
-        int columns = sc.nextInt();
+        int columns = inputValidator(sc);
         System.out.println("Enter the winning row length: ");
-        int winningRowLength = sc.nextInt();
+        int winningRowLength = inputValidator(sc);
         Game game = new Game(rows, columns, winningRowLength); //creates game object with all specifications
         game.playGame(); //object method to start game
         sc.close();
+    }
+
+    public static int inputValidator(Scanner sc){
+        while (true){
+            try{
+                int value = sc.nextInt();
+                if (value > 0){
+                    return value;
+                }
+                System.out.println("Please enter a valid grid number.");
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number.");
+                sc.next();
+            }
+        }
+
     }
 }
