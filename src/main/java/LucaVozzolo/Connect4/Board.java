dc941c487fb22ebfe278d0bcebfd4240b2c48bb5
Lean-Monster <LucaVozzolo16@gmail.com>
2026-04-26 14:22:20 +0100

    Basic menu and classes setup

---
+++ src/main/java/LucaVozzolo/Connect4/Board.java
@@ -0,0 +1,4 @@
+package LucaVozzolo.Connect4;
+
+public class Board {
+}
--- src/main/java/LucaVozzolo/Connect4/Main.java
+++ src/main/java/LucaVozzolo/Connect4/Main.java
@@ -1,7 +1,16 @@
 package LucaVozzolo.Connect4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Connect4!");
+        System.out.println("Enter the number of rows in the grid: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns in the grid: ");
+        int columns = sc.nextInt();
+        System.out.println("Enter the winning row length: ");
+        int winningRowLength = sc.nextInt();
     }
 }
